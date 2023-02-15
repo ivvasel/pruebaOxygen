7b54def876a3e907259475d0d518ce48fb43650d
ivvasel <ivvasel@teleco.upv.es>
2023-02-15 13:16:28 +0100

    Conversiones funcionando

--- app1/src/components/Converter.js
+++ app1/src/components/Converter.js
@@ -1,65 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Save from "./icons/Save";
+import { conversiones } from "../utils/conversiones";
 
 export default function Converter() {
-  const conversiones = [
-    {
-      value: "kmTomillas",
-      name: "kilómetros a millas",
-      from: "km",
-      to: "miles",
-    },
-    {
-      value: "millasTokm",
-      name: "millas a kilómetros",
-      from: "miles",
-      to: "km",
-    },
-    {
-      value: "piesTometros",
-      name: "pies a metros",
-      from: "feet",
-      to: "metres",
-    },
-    {
-      value: "metrosTopies",
-      name: "metros a pies",
-      from: "metres",
-      to: "feet",
-    },
-    {
-      value: "cmTopulgadas",
-      name: "centímetros a pulgadas",
-      from: "cm",
-      to: "inches",
-    },
-    {
-      value: "pulgadasTocm",
-      name: "pulgadas a centímetros",
-      from: "inches",
-      to: "cm",
-    },
-  ];
   const [conversion, setConversion] = useState(conversiones[0]);
+  const [inputUnit, setInputUnit] = useState(0);
+  const [result, setResult] = useState("");
 
-  const handleChange = (value) => {
+  useEffect(() => {
+    selectFormula(conversion);
+    console.log("formula");
+  }, [conversion, inputUnit]);
+
+  const handleChangeSelect = (value) => {
     const selected = conversiones.find((item) => item.value === value);
     setConversion(selected);
   };
+
+  const selectFormula = (item = {}) => {
+    switch (item.value) {
+      case "kmTomiles":
+        setResult(kmTomiles(inputUnit));
+        break;
+      case "milesTokm":
+        setResult(milesTokm(inputUnit));
+        break;
+      case "feetTom":
+        setResult(feetTom(inputUnit));
+        break;
+      case "mTofeet":
+        setResult(mTofeet(inputUnit));
+        break;
+      case "cmToinches":
+        setResult(cmToinches(inputUnit));
+        break;
+      case "inchesTocm":
+        setResult(inchesTocm(inputUnit));
+        break;
+      default:
+        setResult(0);
+    }
+  };
+
+  const kmTomiles = (km) => (km / 1.609).toFixed(2);
+  const milesTokm = (miles) => (miles * 1.609).toFixed(2);
+  const feetTom = (feet) => (feet / 3.281).toFixed(2);
+  const mTofeet = (m) => (m * 3.281).toFixed(2);
+  const cmToinches = (cm) => (cm / 2.54).toFixed(2);
+  const inchesTocm = (inches) => (inches * 2.54).toFixed(2);
 
   return (
     <div name="converter">
       <div name="header-converter">convert</div>
       <div name="body-converter">
         <div>
-          <select onChange={(e) => handleChange(e.target.value)}>
+          <select onChange={(e) => handleChangeSelect(e.target.value)}>
             {conversiones.map((item) => (
               <option value={item.value}>{item.name}</option>
             ))}
           </select>
-          <input name="toConvert"></input>
+          <input
+            name="toConvert"
+            value={inputUnit}
+            onChange={(e) => setInputUnit(e.target.value)}></input>
           {conversion.from}
         </div>
+        <div>{`${result} ${conversion.to}`}</div>
         <div>
           <Save />
         </div>
---
+++ app1/src/utils/conversiones.js
@@ -0,0 +1,38 @@
+export const conversiones = [
+  {
+    value: "kmTomiles",
+    name: "kilómetros a millas",
+    from: "km",
+    to: "miles",
+  },
+  {
+    value: "milesTokm",
+    name: "millas a kilómetros",
+    from: "miles",
+    to: "km",
+  },
+  {
+    value: "feetTom",
+    name: "pies a metros",
+    from: "feet",
+    to: "metres",
+  },
+  {
+    value: "mTofeet",
+    name: "metros a pies",
+    from: "metres",
+    to: "feet",
+  },
+  {
+    value: "cmToinches",
+    name: "centímetros a pulgadas",
+    from: "cm",
+    to: "inches",
+  },
+  {
+    value: "inchesTocm",
+    name: "pulgadas a centímetros",
+    from: "inches",
+    to: "cm",
+  },
+];
